7a8dbf1158294f59a12aefcbbd266b13048c40bd
Alexander Krestinskiy <incloudff@gmail.com>
2024-12-21 15:45:32 +0300

    feat: github issues module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import GithubIssuesModule from './github-issues/github-issues.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV}.env`,
     }),
+    GithubIssuesModule,
   ],
   controllers: [],
   providers: [],
