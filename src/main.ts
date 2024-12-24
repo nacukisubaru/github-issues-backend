ad31e23d412f4dfb91e0819b5eac10964b913c92
Alexander Krestinskiy <incloudff@gmail.com>
2024-12-24 18:09:54 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:*',
+    origin: '*',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
